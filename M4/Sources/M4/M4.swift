0d70362ae3222f7221df8701766287fc8fd48f0f
Paul Uhn <paulu@moj.io>
2021-12-14 14:27:55 -0600

    Add M4 as local package

---
+++ M4/Sources/M4/M4.swift
@@ -0,0 +1,6 @@
+public struct M4 {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ M4/Tests/M4Tests/M4Tests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import M4
+
+final class M4Tests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(M4().text, "Hello, World!")
+    }
+}

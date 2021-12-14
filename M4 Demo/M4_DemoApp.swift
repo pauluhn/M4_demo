7f8af3bfa887601778296760414a9e383a6a5274
Paul Uhn <paulu@moj.io>
2021-12-14 14:24:03 -0600

    Initial commit

---
+++ M4 Demo/M4_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  M4_DemoApp.swift
+//  M4 Demo
+//
+//  Created by Paul U on 12/14/21.
+//
+
+import SwiftUI
+
+@main
+struct M4_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ M4 DemoUITests/M4_DemoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  M4_DemoUITestsLaunchTests.swift
+//  M4 DemoUITests
+//
+//  Created by Paul U on 12/14/21.
+//
+
+import XCTest
+
+class M4_DemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
